6dec41e42eb654997fb05a68d6da4fab2927eb94
Lormond <jangru@mail.ru>
2023-07-26 12:29:21 +0300

    ÐНеÐÐВнесли изменения в Ex02

--- src/main/java/javaExceptions/lesson2/homework/Ex02.java
+++ src/main/java/javaExceptions/lesson2/homework/Ex02.java
@@ -4,16 +4,16 @@ package javaExceptions.lesson2.homework;
 
 public class Ex02 {
 //      int d = 0;     Явное инициирование исключения, я бы убрал!
-        int index = 8;   //Ввели дополнительную переменную, указывающую на желаемый индекс в 7 строке
+        int index = 8;   //Ввели дополнительную переменную, указывающую на желаемый индекс в 9 строке
         if (index >= 0 && index < intArray.length && d != 0) {
             double catchedRes1 = intArray[index] / d; //Имеется явное Arithmetical Exceptions (являющееся unchecked),
             // также возможно IndexOutOfBounds Exceptions, которые необходимо предотвратить в коде
             System.out.println("catchedRes1 = " + catchedRes1);
         }
         else if (d == 0)
-            throw new ArithmeticException("Делить на ноль нельзя. Строка 10: перменная d = " + d);
+            throw new ArithmeticException("Делить на ноль нельзя. Строка 9: перменная d = " + d);
         else
-            throw new IndexOutOfBoundsException("Индекс массива за пределами границ. Строка 10: возможный диапазон" +
+            throw new IndexOutOfBoundsException("Индекс массива за пределами границ. Строка 9: возможный диапазон" +
                     "индексов массива от 0 до " + intArray.length);
 }
  */
